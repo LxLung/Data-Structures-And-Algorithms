5e1564531910286142cc78282e26094309d1325d
Gavin Liang <84223460+LxLung@users.noreply.github.com>
2024-03-25 22:58:33 -0700

    Added Python dir with Arrays.py

---
+++ python/Arrays.py
@@ -0,0 +1,17 @@
+# Arrays in general:
+# 1. Store elements of the same data type
+# 2. Continuous Memory location
+# 3. Indexing references elements
+# 4. Supports higher dimensions
+# List in general:
+# 1. Store elements of varying data types
+# 2. Dynamic sizing
+# 3. Indexing
+# 4. Slicing Ex. my_list[0:2]
+# 5. Supports common operations: Insertion, Deletion, Sorting, Searching.
+# Python default List data type is a List. No surprises here.
+my_list = [0, "a string", True]
+# If the python numpy module is installed, a numpy array can also be used.
+# numpy arrays are not list, but rather Arrays.
+import numpy as np
+
